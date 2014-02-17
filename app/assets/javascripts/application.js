b3184ef134c3fc78fa93cc4dedf224c669ff86f2
RuiMochila <rui.mochila@gmail.com>
2014-02-17 14:29:39 +0000

    Alterei protocolo ws para wss por causa da ssl heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,7 +36,7 @@
 	
 
 
-  var wsUri = "ws://websocket-rails-example.herokuapp.com/websocket";
+  var wsUri = "wss://websocket-rails-example.herokuapp.com/websocket";
   var websocket;
 
   function init()
